2eef2c38513994c12d0e8f06987f0383b44fc11b
berkbasarir <bbasarir@gmail.com>
2021-02-07 14:40:25 +0300

    BB

---
+++ HW6SQLQuery.sql
@@ -0,0 +1,8 @@
+﻿select pr.ProductID
+       ,pr.ProductName
+	   ,sum(od.UnitPrice*od.Quantity) AS 'Kazanılan Toplam Miktar' 
+from Products as pr 
+inner join [Order Details] as od on pr.ProductID=od.ProductID
+inner join Orders as o on od.OrderID=o.OrderID
+group by pr.ProductID,pr.ProductName 
+order by pr.ProductID
